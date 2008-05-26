eb46417a6163e8dde8146c0d90d7aeb4ea74099c
wgw <tjm1100@gmail.com>
2008-05-25 23:54:08 -0500

    add Override support for audio mapping

--- plugins/video/transcode.py
+++ plugins/video/transcode.py
@@ -593,6 +593,8 @@ def video_info(inFile):
         if key.startswith('Override_'):
             vInfo['Supported'] = True
             vInfo[key.replace('Override_','')] = metadata[key]
+            if type(vInfo['mapAud']) != list:
+                vInfo[key.replace('Override_','')] = [config.str2tuple(metadata[key])]
 
     info_cache[inFile] = (mtime, vInfo)
     logger.debug("; ".join(["%s=%s" % (k, v) for k, v in vInfo.items()]))
