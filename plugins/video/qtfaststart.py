3d710c80285468fba1a96023a8c9892f69c057a4
William McBrine <wmcbrine@gmail.com>
2009-06-07 19:32:06 -0400

    Avoid unnecessary copies.

--- plugins/video/qtfaststart.py
+++ plugins/video/qtfaststart.py
@@ -129,7 +129,9 @@ def find_atoms(size, datastream):
 def output(outfile, offset, data):
     global count
     length = len(data)
-    if count + length > offset:
+    if count > offset:
+        outfile.write(data)
+    elif count + length > offset:
         outfile.write(data[offset - count:])
     count += length
 
