a0347d476e15c76fcb913dbf47e5ae8f4bf10943
William McBrine <wmcbrine@gmail.com>
2009-07-25 00:25:08 -0400

    Read video MP4 metadata. By "Minckster", modified by me.

--- metadata.py
+++ metadata.py
@@ -5,6 +5,7 @@ import subprocess
 from datetime import datetime
 from xml.dom import minidom
 
+import mutagen
 from lrucache import LRUCache
 
 import config
@@ -13,6 +14,7 @@ import config
 TRIBUNE_CR = ' Copyright Tribune Media Services, Inc.'
 
 tivo_cache = LRUCache(50)
+mp4_cache = LRUCache(50)
 
 def tag_data(element, tag):
     for name in tag.split('/'):
@@ -40,6 +42,76 @@ def _tag_value(element, tag):
         name = item[0].firstChild.data
         return name[0] + value[0]
 
+def from_moov(full_path):
+    if full_path in mp4_cache:
+        return mp4_cache[full_path]
+
+    metadata = {}
+    len_desc = 0
+
+    try:
+        mp4meta = mutagen.File(full_path)
+        assert(mp4meta)
+    except:
+        mp4_cache[full_path] = {}
+        return {}
+
+    for key, value in mp4meta.items():
+        # The following 1-to-1 correspondence of atoms to pyTivo
+        # variables is TV-biased
+        keys = {'tvnn': 'callsign', 'tven': 'episodeNumber',
+                'tvsh': 'seriesTitle'}
+        if type(value) == list:
+            value = value[0]
+        if key == 'stik':
+            metadata['isEpisode'] = ['false', 'true'][value == 'TV Show']
+        elif key in keys:
+            metadata[keys[key]] = value
+        # These keys begin with the copyright symbol \xA9
+        elif key == '\xa9day':
+            if len(value) == 4:
+                value += '-01-01T16:00:00Z'
+            metadata['originalAirDate'] = value
+            #metadata['time'] = value
+        elif key in ['\xa9gen', 'gnre']:
+            for k in ('vProgramGenre', 'vSeriesGenre'):
+                if k in metadata:
+                    metadata[k].append(value)
+                else:
+                    metadata[k] = [value]
+        elif key == '\xa9nam':
+            if 'tvsh' in mp4meta:
+                metadata['episodeTitle'] = value
+            else:
+                metadata['title'] = value
+
+        # Description in desc, cmt, and/or ldes tags. Keep the longest.
+        elif key in ['desc', '\xa9cmt', 'ldes'] and len(value) > len_desc:
+            metadata['description'] = value
+            len_desc = len(value)
+
+        # A common custom "reverse DNS format" tag
+        # Possible TV values: TV-Y7 TV-Y TV-G TV-PG TV-14 TV-MA Unrated
+        # Possible MPAA values: G PG PG-13 R NC-17 Unrated
+        elif (key == '----:com.apple.iTunes:iTunEXTC' and
+              ('us-tv' in value or 'mpaa' in value)):
+            ratings = {'TV-Y7': 'x1', 'TV-Y': 'x2', 'TV-G': 'x3',
+                       'TV-PG': 'x4', 'TV-14': 'x5', 'TV-MA': 'x6',
+                       'Unrated': 'x7', 'G': 'G1', 'PG': 'P2',
+                       'PG-13': 'P3', 'R': 'R4', 'NC-17': 'N6'}
+            rating = value.split("|")[1]
+            if rating in ratings:
+                if 'us-tv' in value:
+                    metadata['tvRating'] = ratings[rating]
+                elif 'mpaa' in value:
+                    metadata['mpaaRating'] = ratings[rating]
+
+        # Actors, directors, producers, AND screenwriters may be in a long
+        # embedded XML plist, with key '----' and rDNS 'iTunMOVI'. Ughh!
+
+    mp4_cache[full_path] = metadata
+    return metadata
+
 def from_text(full_path):
     metadata = {}
     path, name = os.path.split(full_path)
@@ -60,15 +132,17 @@ def from_text(full_path):
     return metadata
 
 def basic(full_path):
-    base_path, title = os.path.split(full_path)
+    base_path, name = os.path.split(full_path)
+    title, ext = os.path.splitext(name)
     mtime = os.stat(full_path).st_mtime
     if (mtime < 0):
         mtime = 0
     originalAirDate = datetime.fromtimestamp(mtime)
 
-    metadata = {'title': '.'.join(title.split('.')[:-1]),
+    metadata = {'title': title,
                 'originalAirDate': originalAirDate.isoformat()}
-
+    if ext.lower() in ['.mp4', '.m4v', '.mov']:
+        metadata.update(from_moov(full_path))
     metadata.update(from_text(full_path))
 
     return metadata
@@ -166,8 +240,13 @@ def from_tivo(full_path):
 if __name__ == '__main__':        
     import sys
     if len(sys.argv) > 1:
-        config.init([])
-        metadata = from_tivo(sys.argv[1])
+        metadata = {}
+        ext = os.path.splitext(sys.argv[1])[1].lower()
+        if ext == '.tivo':
+            config.init([])
+            metadata.update(from_tivo(sys.argv[1]))
+        elif ext in ['.mp4', '.m4v', '.mov']:
+            metadata.update(from_moov(sys.argv[1]))
         for key in metadata:
             value = metadata[key]
             if type(value) == list:
