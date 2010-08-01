c7bfa1f69783b55ca3be2b3159827b1a7785e625
William McBrine <wmcbrine@gmail.com>
2010-08-01 07:40:28 -0400

    Explicit Python version check and report.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import sys
+
+if sys.version_info[0] != 2 or sys.version_info[1] < 4:
+    print ('ERROR: pyTivo requires Python >= 2.4, < 3.0.\n')
+    sys.exit(1)
+
 import ConfigParser
 import getopt
 import logging
@@ -7,7 +13,6 @@ import re
 import random
 import socket
 import string
-import sys
 from ConfigParser import NoOptionError
 
 guid = ''.join([random.choice(string.letters) for i in range(10)])
