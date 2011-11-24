8b37451ae048248f88f27577e520563a064ff4d4
lucas_nz <lucas_nz@hotmail.com>
2011-11-24 23:02:16 +1300

    Support putting pyTivo.conf in "All Users\Application Data\pyTivo" on XP and "ProgramData\pyTivo" on Vista and Win7

--- config.py
+++ config.py
@@ -14,8 +14,26 @@ guid = ''.join([random.choice(string.letters) for i in range(10)])
 our_ip = ''
 config = ConfigParser.ConfigParser()
 
+config_win_default = ''
+
+if sys.platform == "win32":
+    import _winreg
+
+    try:
+        explorerFolders = _winreg.OpenKey(
+        _winreg.HKEY_LOCAL_MACHINE,
+        'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        )
+
+        winCommonAppDataVal, winCommonAppDataType = _winreg.QueryValueEx(explorerFolders, 'Common AppData')
+
+        config_win_default = os.path.join(winCommonAppDataVal, 'pyTivo', 'pyTivo.conf')
+
+    except WindowsError:
+        print "Can't access Windows Registry to find common Application Data path."
+
 p = os.path.dirname(__file__)
-config_files = ['/etc/pyTivo.conf', os.path.join(p, 'pyTivo.conf')]
+config_files = ['/etc/pyTivo.conf', config_win_default, os.path.join(p, 'pyTivo.conf')]
 configs_found = []
 
 tivos = {}
